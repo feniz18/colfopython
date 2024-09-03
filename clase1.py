f0ebc7f1341fb3d63b323a3ed336d7baf4803c01
David <fenizhc18@gmail.com>
2024-09-03 12:06:24 -0500

    Commit inicial

---
+++ clase1.py
@@ -0,0 +1,15 @@
+''' 
+Este programa calcula la sumatoria de dos numeros
+variable 1
+variable 2
+'''
+
+num1 = int(input("Por favor ingrese el primer numero"))
+num2 = int(input("Por favor ingrese el segundo numero"))
+
+sumatoria = num1 + num2
+
+print(sumatoria)
+
+''' comentario agregado'''
+
