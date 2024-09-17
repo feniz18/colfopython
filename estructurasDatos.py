c58a635cbe9596e48b6d7496456aa4bb0a1e7067
David <fenizhc18@gmail.com>
2024-09-17 12:06:13 -0500

    Clase estructura de datos listas y tuplas

---
+++ estructurasDatos.py
@@ -0,0 +1,18 @@
+#Tuplas y listas 
+
+#Tuplas: No son modificables= Inmutables
+#listas: Permiten modificar los datos originas: Mutables
+
+#tuplas caracteristica ()
+
+tupla = ('David', 'Billy', 'Adriana', 'Daniel','Adriana')
+
+
+#listas
+
+lista = ['David', 'Billy', 'Adriana', 'Daniel','Adriana']
+lista2 = ['Milton', 'JOse']
+lista.extend(lista2)
+lista.pop()
+
+print(lista)
