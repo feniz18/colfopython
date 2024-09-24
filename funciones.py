70b3ef5421adb704eb43d84ddd0db365607b3d17
David <fenizhc18@gmail.com>
2024-09-24 12:17:01 -0500

    Clase 7: Funciones: Codigo sin funciones

---
+++ funciones.py
@@ -0,0 +1,41 @@
+variable1 = 5
+variable2 = 6
+
+resultado = variable1 + variable2
+if(variable1<0):
+    print("no puedes ingresar valores postivo")
+else:
+    print(resultado)
+
+variable1 = 3
+variable2 = 4
+
+resultado = variable1 - variable2
+print(resultado)
+
+if(variable1<0):
+    print("no puedes ingresar valores postivo")
+else:
+    print(resultado)
+
+variable1 = 3
+variable2 = 4
+
+resultado = variable1 * variable2
+print(resultado)
+
+if(variable1<0):
+    print("no puedes ingresar valores postivo")
+else:
+    print(resultado)
+
+variable1 = 3
+variable2 = 4
+
+resultado = variable1 / variable2 
+print(resultado)
+
+if(variable1<0):
+    print("no puedes ingresar valores postivo")
+else:
+    print(resultado)
